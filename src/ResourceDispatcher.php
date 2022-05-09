23198c902366926604f2f561dd7f68762f5fcf62
Romano Schoonheim <stal@romanoschoonheim.nl>
2022-05-09 08:08:16 +0200

    development: Resource dispatcher.

---
+++ src/ResourceDispatcher.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Rschoonheim\LaravelApiResource;
+
+/**
+ * class ResourceDispatcher.
+ *
+ * @package Rschoonheim\LaravelApiResource;
+ */
+class ResourceDispatcher
+{
+
+}
