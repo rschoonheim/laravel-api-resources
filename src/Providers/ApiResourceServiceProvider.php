91c3a79ec30ecd3e26a6fc3dbd8c8cc814e8d363
Romano Schoonheim <romano@romanoschoonheim.nl>
2022-05-09 06:57:02 +0200

    master: Api resource service provider.

---
+++ src/Providers/ApiResourceServiceProvider.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Rschoonheim\LaravelApiResource\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * class ApiResourceServiceProvider.
+ */
+class ApiResourceServiceProvider extends ServiceProvider
+{
+}
